c6e76bf43dcf35c6e16462c039c1a55787db01e5
gregoryesrig24 <gesrig@nd.edu>
2017-04-12 11:46:38 -0400

    This is the ToolBar stuff

--- app/src/main/java/com/example/gregoryesrig24/finalproject/SpecItem.java
+++ app/src/main/java/com/example/gregoryesrig24/finalproject/SpecItem.java
@@ -2,23 +2,27 @@ package com.example.gregoryesrig24.finalproject;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.GridLayout;
 
 import static com.example.gregoryesrig24.finalproject.R.id.toolbar;
 
 /**
- * Created by gregoryesrig24 on 4/10/17.
+ * Created by gregoryesrig24 on 4/11/17.
  */
 
-public class GetProfile extends AppCompatActivity {
+public class SpecItem extends AppCompatActivity{
+
+
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_getprofile);
+        setContentView(R.layout.activity_specitem);
 
         Toolbar actionBarToolbar = (Toolbar) findViewById(toolbar);
         setSupportActionBar(actionBarToolbar);
@@ -26,6 +30,7 @@ public class GetProfile extends AppCompatActivity {
         getSupportActionBar().setTitle("");
 
     }
+
 
     public boolean onCreateOptionsMenu (Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
@@ -38,7 +43,7 @@ public class GetProfile extends AppCompatActivity {
         int res_id = item.getItemId();
 
         if (res_id == R.id.settings) {
-            startActivity(new Intent(GetProfile.this, MainActivity.class));
+            startActivity(new Intent(SpecItem.this, MainActivity.class));
         }
 
         return true;
